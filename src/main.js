5029b4ecd1d5d086e065f3316640a94185cd7b19
me <me@me.me.com>
2020-08-19 12:01:08 -0700

    add basic ui

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import $ from 'jquery';
 import 'bootstrap';
+import './styles.css';
+import { Game } from './tetris.js';
+
+$(document).ready(function () {
+  $('#start').click(function () {
+    let Tetris = new Game();
+
+    
+  });
+});
